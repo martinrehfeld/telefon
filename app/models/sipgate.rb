47ec8aebbee152cef7e914942cf34deca5a3bdb2
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-07-01 19:55:54 +0200

    basic Sipgate API functionality

--- app/models/sipgate.rb
+++ app/models/sipgate.rb
@@ -1,15 +1,38 @@
+require 'xmlrpc/client'
+require 'singleton'
+
 class Sipgate
   
-  @config = YAML.load_file(File.join(Rails.root,'config','sipgate.yml'))
-  @username = @config['username']
-  @password = @config['password']
+  include Singleton
   
-  def self.username
-    @username
+  CLIENT_ID = { 'ClientName' => 'telefon', 'ClientVersion' => "1.0", 'ClientVendor' => 'GL Networks, Berlin'}
+
+  attr_accessor :server
+  
+  def initialize
+    config = YAML.load_file(File.join(Rails.root,'config','sipgate.yml'))
+    @server = XMLRPC::Client.new2("https://#{config['username']}:#{config['password']}@samurai.sipgate.net/RPC2")
   end
   
-  def self.password
-    @password
+  def identify
+    return_hash @server.call("samurai.ClientIdentify", CLIENT_ID)
   end
   
+  def voice_call(local_uri, remote_uri)
+    return_hash @server.call("samurai.SessionInitiate", {
+      'LocalUri'  => local_uri,
+      'RemoteUri' => remote_uri,
+      'TOS'       => 'voice',
+      'Content'   => ''
+    })
+  end
+  
+private
+
+  def return_hash(h)
+    returning({}) do |new_hash|
+      h.each {|k,v| new_hash[k.underscore.to_sym] = v }
+    end
+  end
+
 end
--- spec/models/sipgate_spec.rb
+++ spec/models/sipgate_spec.rb
@@ -1,8 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Sipgate do
-  it "should provide account information from config/sipgate.yml" do
-    Sipgate.username.should_not be_empty
-    Sipgate.password.should_not be_empty
+  before(:each) do
+     @sipgate = Sipgate.instance
+     @mock_server = mock("xmlrpc-server")
+  end
+
+  it "should create XMLRPC-Server" do
+    @sipgate.server.should be_kind_of(XMLRPC::Client)
+  end
+  
+  it "should read account information from config/sipgate.yml" do
+    @sipgate.server.user.should_not be_empty
+    @sipgate.server.password.should_not be_empty
+  end
+  
+  it "should identify itself successfully " do
+    @sipgate.server = @mock_server
+    @mock_server.should_receive(:call).with("samurai.ClientIdentify", Sipgate::CLIENT_ID).once.and_return({'StatusCode' => 200})
+    @sipgate.identify[:status_code].should == 200
+  end
+  
+  it "should initiate a voice session properly" do
+    from, to = 'sip:123@sipgate.de', 'sip:456@sipgate.de'
+    @sipgate.server = @mock_server
+    @mock_server.should_receive(:call).with("samurai.SessionInitiate", { 'LocalUri' => from, 'RemoteUri' => to, 'TOS' => 'voice', 'Content' => ''}).once.and_return({'StatusCode' => 200})
+    @sipgate.voice_call(from, to)[:status_code].should == 200
   end
 end
