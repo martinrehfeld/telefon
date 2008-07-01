f402b26c8909dc0e3cebbe5546e4b0cd67535e31
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-07-01 19:03:59 +0200

    implemented config/sipgate.yml loading

--- app/models/sipgate.rb
+++ app/models/sipgate.rb
@@ -1,2 +1,15 @@
-class Sipgate < ActiveRecord::Base
+class Sipgate
+  
+  @config = YAML.load_file(File.join(Rails.root,'config','sipgate.yml'))
+  @username = @config['username']
+  @password = @config['password']
+  
+  def self.username
+    @username
+  end
+  
+  def self.password
+    @password
+  end
+  
 end
