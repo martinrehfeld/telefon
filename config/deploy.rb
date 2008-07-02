20eb14f5e4b31f64494acad788e249e83e1a1707
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-07-02 14:58:13 +0200

    capified, example sipgate.yml

---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "telefon"
+
+set :repository,  "git@github.com:martinrehfeld/telefon.git"
+set :scm, "git"
+
+set :deploy_via, :remote_cache
+set :deploy_to, "/var/rails/#{application}"
+
+set :ssh_options, { :forward_agent => true }
+set :user, "www-data"            # defaults to the currently logged in user
+set :use_sudo, false             # do not use sudo
+
+role :app, "media.local"
+role :web, "media.local"
+role :db,  "media.local", :primary => true
+
+namespace :deploy do
+  
+  desc "Restart Mongrels, leave Apache running"
+  task :restart, :roles => :app do
+    sudo "/etc/init.d/mongrel_cluster restart"
+  end
+
+  desc "Cold-start Mongrels, Apache is assumed to be running"
+  task :start, :roles => :app do
+    sudo "/etc/init.d/mongrel_cluster start"
+  end
+  
+  desc "Stop Mongrels, Apache is left running"
+  task :stop, :roles => :app do
+    sudo "/etc/init.d/mongrel_cluster stop"
+  end
+
+end
