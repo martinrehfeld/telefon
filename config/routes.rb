61d512edd3ad4f1998bebe2f59971cca615fc9d9
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-07-14 00:59:27 +0200

    DRYed up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.calls          '',                  :controller => 'calls', :action => 'index', :conditions => { :method => :get }
-  map.calls          '',                  :controller => 'calls', :action => 'create', :conditions => { :method => :post }
-  map.call_history   'call-history',      :controller => 'calls', :action => 'history', :conditions => { :method => :get }
-  map.call_behaviour 'call-behaviour.js', :controller => 'calls', :action => 'behaviour', :format => :js, :conditions => { :method => :get }
+  map.with_options :controller => 'calls' do |route|
+    route.calls          '',                  :action => 'index', :conditions => { :method => :get }
+    route.calls          '',                  :action => 'create', :conditions => { :method => :post }
+    route.call_history   'call-history',      :action => 'history', :conditions => { :method => :get }
+    route.call_behaviour 'call-behaviour.js', :action => 'behaviour', :format => :js, :conditions => { :method => :get }
+  end
 
 end
