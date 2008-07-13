24ae8ab36dae508adfeccae57df934c0fcd52df0
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2008-07-14 00:48:48 +0200

    narrowed routing, sticking with root URL even when submitting calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.resources :calls, :collection => { :history   => :get,
-                                         :behaviour => :get }
-  map.root :calls
+  map.calls          '',                  :controller => 'calls', :action => 'index', :conditions => { :method => :get }
+  map.calls          '',                  :controller => 'calls', :action => 'create', :conditions => { :method => :post }
+  map.call_history   'call-history',      :controller => 'calls', :action => 'history', :conditions => { :method => :get }
+  map.call_behaviour 'call-behaviour.js', :controller => 'calls', :action => 'behaviour', :format => :js, :conditions => { :method => :get }
 
 end
---
+++ spec/controllers/calls_routing_spec.rb
@@ -0,0 +1,43 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe CallsController do
+  describe "route generation" do
+
+    # we expect calls to be the root route
+    it "should map { :controller => 'calls', :action => 'index' } to /" do
+      route_for(:controller => "calls", :action => "index").should == "/"
+    end
+  
+    it "should map { :controller => 'calls', :action => 'create' } to /" do
+      route_for(:controller => "calls", :action => "create").should == "/"
+    end
+
+    it "should map { :controller => 'calls', :action => 'history' } to /call-history" do
+      route_for(:controller => "calls", :action => "history").should == "/call-history"
+    end
+
+    it "should map { :controller => 'calls', :action => 'behaviour', :format => :js } to /call-behaviour.js" do
+      route_for(:controller => "calls", :action => "behaviour", :format => :js).should == "/call-behaviour.js"
+    end
+  end
+
+  describe "route recognition" do
+
+    # we expect calls to be the root route
+    it "should generate params { :controller => 'calls', action => 'index' } from GET /" do
+      params_from(:get, "/").should == {:controller => "calls", :action => "index"}
+    end
+
+    it "should generate params { :controller => 'calls', action => 'create' } from POST /" do
+      params_from(:post, "/").should == {:controller => "calls", :action => "create"}
+    end
+
+    it "should generate params { :controller => 'calls', action => 'history' } from GET /call-history" do
+      params_from(:get, "/call-history").should == {:controller => "calls", :action => "history"}
+    end
+
+    it "should generate params { :controller => 'calls', action => 'behaviour', :format => :js } from GET /call-behaviour.js" do
+      params_from(:get, "/call-behaviour.js").should == {:controller => "calls", :action => "behaviour", :format => :js}
+    end
+  end
+end
